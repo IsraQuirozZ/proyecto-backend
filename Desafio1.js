81ebe0d2e58502df7fce9ad60275e64c72db4858
Israel <isra_quirozz@hotmail.com>
2023-04-24 22:01:42 +0200

    Complete sprint-2

--- Desafio1.js
+++ Desafio1.js
@@ -1,6 +1,21 @@
+const fs = require("fs");
+
 class ProductManager {
   constructor() {
     this.products = [];
+    this.path = "./data/products.json";
+    this.init(this.path);
+  }
+
+  init(path) {
+    let file = fs.existsSync(path);
+    if (!file) {
+      fs.writeFileSync(path, "[]");
+      return console.log("File created");
+    } else {
+      this.products = JSON.parse(fs.readFileSync(path, "utf-8"));
+      return console.log("Data recovered");
+    }
   }
 
   getProducts() {
@@ -9,15 +24,15 @@ class ProductManager {
   }
 
   getProductById(productId) {
-    let foundedProduct = this.products.find(
+    let productFound = this.products.find(
       (product) => product.id === productId
     );
-    if (foundedProduct) {
-      console.log(foundedProduct);
+    if (productFound) {
+      console.log(productFound);
     } else {
-      console.log(`Product ${productId} not founded.`);
+      console.log(`Product with Id: ${productId} not found.`);
     }
-    return foundedProduct;
+    return productFound;
   }
 
   addProduct({ title, description, price, thumbnail, code, stock }) {
@@ -25,7 +40,7 @@ class ProductManager {
     let codes = [];
     // Validar que todos los campos sean obligatorios
     if (!title || !description || !price || !thumbnail || !code || !stock) {
-      console.log("All fields are required");
+      console.log("error: all fields are required");
     } else {
       // Id autoincrementable
       if (!this.products.length) {
@@ -37,18 +52,89 @@ class ProductManager {
       this.products.forEach((prod) => codes.push(prod["code"]));
       let product = { title, description, price, thumbnail, code, stock, id };
       // Códigos repetidos no se agregan
-      if (codes.includes(product.code)) {
-        console.log(
-          `Product ${product.id} will not be added because it has an existing code:  ${product.code}.`
-        );
-      } else {
+      if (!codes.includes(product.code)) {
         this.products.push(product);
+        let dataJson = JSON.stringify(this.products, null, 2);
+        fs.promises
+          .writeFile(this.path, dataJson)
+          .then((res) => console.log(`Product #${product.id} added`))
+          .catch((err) => console.log(err));
+      } else {
+        console.log(
+          `We can't add this product because it has a repeated code: ${product.code}`
+        );
       }
+    }
+  }
+
+  updateProduct(productId, data) {
+    let productFound = this.getProductById(productId);
+    if (!productFound) {
+      console.log("error: not found user to update");
+      return "error: not found user to update";
+    }
+
+    // Verificar si la data no está vacía
+    if (Object.keys(data).length === 0 || typeof data !== "object") {
+      console.log("error: data is required");
+      return "error: data is required";
+    }
+
+    for (let prop in data) {
+      //Verificar que la propiedad pertenece al objeto (propiedad que exista)
+      for (let prop in data) {
+        if (
+          prop === "title" ||
+          prop === "description" ||
+          prop === "price" ||
+          prop === "thumbnail" ||
+          prop === "stock"
+        ) {
+        } else if (prop === "code" || prop === "id") {
+          console.log(`error: you can't modify the ${prop} of a product`);
+          return `error: you can't modify the ${prop} of a product`;
+        } else {
+          console.log(`error: "${prop}" is not a property of product`);
+          return `error: "${prop}" is not a property of product`;
+        }
+      }
+      productFound[prop] = data[prop];
+    }
+
+    let dataJson = JSON.stringify(this.products, null, 2);
+    fs.promises
+      .writeFile(this.path, dataJson)
+      .then((res) => console.log(`Updated product: ${productFound.id}`))
+      .catch((err) => console.log(err));
+  }
+
+  deleteProduct(productId) {
+    let productFound = this.getProductById(productId);
+
+    if (!productFound) {
+      console.log("error: not found user to delete");
+      return "error: not found user to delete";
+    } else {
+      this.products = this.products.filter(
+        (prod) => prod.id !== productFound.id
+      );
+      let dataJson = JSON.stringify(this.products, null, 2);
+      fs.promises
+        .writeFile(this.path, dataJson)
+        .then((res) => {
+          console.log(
+            `The product with id: ${productId} has been deleted successfully`
+          );
+        })
+        .catch((err) => console.log(err));
     }
   }
 }
 
 let product = new ProductManager();
+// Se creará el archivo JSON con un array vacío
+
+product.getProducts(); // Array vacío
 
 product.addProduct({
   title: "Producto prueba",
@@ -58,7 +144,6 @@ product.addProduct({
   code: "abc123",
   stock: 25,
 });
-
 product.addProduct({
   title: "Producto prueba",
   description: "Este es un producto prueba",
@@ -68,15 +153,7 @@ product.addProduct({
   stock: 25,
 });
 
-// Este producto no se agrega
-product.addProduct({
-  title: "Producto prueba",
-  description: "Este es un producto prueba",
-  price: 200,
-  thumbnail: "Sin imagen",
-  code: "abc123",
-  stock: 25,
-});
-
-product.getProducts();
-product.getProductById(3);
+product.getProductById(1); // Objeto anterior
+product.getProductById(3); // Error (no existe el producto con id 3)
+// product.updateProduct(1, { title: "Producto actualizado" }); // Actualizará el producto con id 1
+product.deleteProduct(1); // Se eliminará el producto y el archivo JSON quedará vacío
