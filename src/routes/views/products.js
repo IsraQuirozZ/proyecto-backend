5c367f07db0c52c92ff7692486064d9e6e080922
Israel <isra_quirozz@hotmail.com>
2023-05-13 19:39:06 +0200

    GET /products view

--- src/routes/views/products.js
+++ src/routes/views/products.js
@@ -3,15 +3,33 @@ import products_api_router from "../api/products.js";
 
 const router = Router();
 
+async function getProducts() {
+  return await fetch("http://localhost:8080/api/products")
+    .then((res) => res.json())
+    .then((data) => data.response);
+}
+
 router.get("/products", async (req, res, next) => {
   try {
     return res.render("products", {
       title: "Products",
-      products: [{ title: "1" }, { title: "2" }, { title: "3" }],
+      products: await fetch("http://localhost:8080/api/products")
+        .then((res) => res.json())
+        .then((data) => data.response),
     });
   } catch (error) {
     next(error);
   }
 });
+
+// {
+//   "title": "Producto 1",
+//   "description": "Este es un producto prueba",
+//   "price": 200,
+//   "thumbnail": "Sin imagen",
+//   "code": "abc123",
+//   "stock": 25,
+//   "id": 1
+// },
 
 export default router;
