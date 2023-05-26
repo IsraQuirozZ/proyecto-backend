3b31d90a50e477f2496ed98884206ef423ddd2f7
Israel <isra_quirozz@hotmail.com>
2023-05-26 23:56:51 +0200

    Start Chat Styles

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,30 +1,35 @@
 let userName;
 
 Swal.fire({
-    title: 'Write your name',
-    input: 'text',
-    inputValidator: value => !value && 'Please, write your name',
-    allowOutsideClick: false,
-    allowEscapeKey: false
-}).then(res => {
-    userName = res.value
-    document.querySelector('#userName').innerHTML = userName
-    socket.emit('auth', userName)
-})
+  title: "Write your name",
+  input: "text",
+  inputValidator: (value) => !value && "Please, write your name",
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+}).then((res) => {
+  userName = res.value;
+  document.querySelector("#userName").innerHTML = userName;
+  socket.emit("auth", userName);
+});
 
-const send = e => {
-    if (e.key === 'Enter'){
-        socket.emit('new_message', {
-            userName,
-            message: chatBox.value
-        })
-        chatBox.value = ''
-    }
-}
+const send = (e) => {
+  if (e.key === "Enter") {
+    socket.emit("new_message", {
+      userName,
+      message: chatBox.value,
+    });
+    chatBox.value = "";
+  }
+};
 
-const chatBox = document.querySelector('#chatBox')
-chatBox.addEventListener('keyup', send)
+const chatBox = document.querySelector("#chatBox");
+chatBox.addEventListener("keyup", send);
 
-socket.on('all_messages', data => {
-    document.querySelector('#messages').innerHTML = data.map(msg => `<h6>${msg.userName}</h6><p>${msg.message}</p>`).join('')
-})
+socket.on("all_messages", (data) => {
+  document.querySelector("#messages").innerHTML = data
+    .map(
+      (msg) =>
+        `<div class="message"><h6>${msg.userName}</h6><p>${msg.message}</p></div>`
+    )
+    .join("");
+});
