3ef2f86734cb73d1ef77f6d126f27c9228fe283e
Guada <110698501+Guada-pdls@users.noreply.github.com>
2023-05-26 19:23:27 -0300

    Piva 5 (#24)
    * handlebars
    
    * products handlebars (#14)
    
    * html form
    
    * error form
    
    * Solución form, cart quantity
    
    * Quiroz 5 (#16)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * finish
    
    * Piva 5 (#19)
    
    * error form
    
    * Solución form, cart quantity
    
    * finish
    
    * Quiroz 5 (#20)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Quiroz 5 (#21)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Fix styles
    
    * estilos
    
    ---------
    
    Co-authored-by: Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -12,24 +12,29 @@ Swal.fire({
   socket.emit("auth", userName);
 });
 
-const send = (e) => {
-  if (e.key === "Enter") {
-    socket.emit("new_message", {
-      userName,
-      message: chatBox.value,
-    });
-    chatBox.value = "";
-  }
-};
+const send = e => {
+    if (e.key === 'Enter' && chatBox.value !== ''){
+        socket.emit('new_message', {
+            userName,
+            message: chatBox.value.toString()
+        })
+        chatBox.value = ''
+    }
+}
 
-const chatBox = document.querySelector("#chatBox");
-chatBox.addEventListener("keyup", send);
+socket.on('all_messages', data => {
+    document.querySelector('#messages').innerHTML = data.map(msg => {
 
-socket.on("all_messages", (data) => {
-  document.querySelector("#messages").innerHTML = data
-    .map(
-      (msg) =>
-        `<div class="message"><h6>${msg.userName}</h6><p>${msg.message}</p></div>`
-    )
-    .join("");
-});
+        // Creo los nodos y les inserto el contenido en texto (evito que inserten etiquetas)
+        const msgElement = document.createElement('p')
+        msgElement.appendChild(document.createTextNode(msg.message))
+
+        const userNameElement = document.createElement('h6')
+        userNameElement.appendChild(document.createTextNode(msg.userName))
+
+        const container = document.createElement('div') // Creo el nodo contenedor
+        container.append(userNameElement, msgElement)   // Agrego el username y el msg
+
+        return container.innerHTML // Retorno el HTML
+    }).join('')
+})
