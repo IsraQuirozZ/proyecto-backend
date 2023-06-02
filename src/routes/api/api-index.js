3b9d92c638b40bcbfa8e53b5dc87912219b7e219
Israel <isra_quirozz@hotmail.com>
2023-06-02 20:45:39 +0200

    Complete

--- src/routes/api/api-index.js
+++ src/routes/api/api-index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 // import products_router from "./products.js";
-import products_router from "./products.mongo.js";
+import products_router from "./products.mongo.js"; // mongo
 import cart_router from "./carts.js";
 
 const router = Router();
