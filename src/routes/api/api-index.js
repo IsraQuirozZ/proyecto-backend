639c3214176f03c1bf12efaa7a3cc9e46d9bd842
Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
2023-06-03 03:31:43 +0200

    Quiroz 5 (#27)
    * Complete Desarrollador 1

--- src/routes/api/api-index.js
+++ src/routes/api/api-index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import product_router from "./products.js";
+// import products_router from "./products.js";
+import products_router from "./products.mongo.js"; // mongo
 import cart_router from "./carts.js";
 
 const router = Router();
 
-router.use("/products", product_router);
+router.use("/products", products_router);
 router.use("/carts", cart_router);
 
 export default router;
