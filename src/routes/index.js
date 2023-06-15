360a0a355a97718f31117af1eb845cbbe95a94d1
Guada <guadapdls@gmail.com>
2023-06-15 20:54:07 -0300

    limpieza views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import api_router from "./api/api-index.js";
-import views_router from './views/view-index.js'
 
 const router = Router();
 
 router.use("/api", api_router);
-router.use('/', views_router)
 
 export default router;
