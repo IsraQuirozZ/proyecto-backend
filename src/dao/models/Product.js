7d38accb15468ba73d028c861e3677482a40210f
Guada <110698501+Guada-pdls@users.noreply.github.com>
2023-06-21 15:24:28 -0300

    Sprint 7 (#33)
    * handlebars
    
    * products handlebars (#14)
    
    * html form
    
    * error form
    
    * Solución form, cart quantity
    
    * Quiroz 5 (#16)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * finish
    
    * Piva 5 (#19)
    
    * error form
    
    * Solución form, cart quantity
    
    * finish
    
    * Quiroz 5 (#20)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Quiroz 5 (#21)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Fix styles
    
    * estilos
    
    * estilo chat
    
    * Quiroz 5 (#27)
    
    * Complete Desarrollador 1
    
    * persistencia mongo
    
    * vista
    
    * cambio modelo cart
    
    * paginate products
    
    * Quiroz 7 (#31)
    
    * Sprint 6 (#30)
    
    * Quiroz 5 (#27)
    
    * Complete Desarrollador 1
    
    * Piva 5 (#28)
    
    * handlebars
    
    * products handlebars (#14)
    
    * html form
    
    * error form
    
    * Solución form, cart quantity
    
    * Quiroz 5 (#16)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * finish
    
    * Piva 5 (#19)
    
    * error form
    
    * Solución form, cart quantity
    
    * finish
    
    * Quiroz 5 (#20)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Quiroz 5 (#21)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Fix styles
    
    * estilos
    
    * estilo chat
    
    * persistencia mongo
    
    ---------
    
    Co-authored-by: Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
    
    * Piva 5 (#29)
    
    * handlebars
    
    * products handlebars (#14)
    
    * html form
    
    * error form
    
    * Solución form, cart quantity
    
    * Quiroz 5 (#16)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * finish
    
    * Piva 5 (#19)
    
    * error form
    
    * Solución form, cart quantity
    
    * finish
    
    * Quiroz 5 (#20)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Quiroz 5 (#21)
    
    * products handlebars
    
    * GET /products view
    
    * Complete quiroz-5
    
    * Changes in data
    
    * Fix styles
    
    * estilos
    
    * estilo chat
    
    * persistencia mongo
    
    * vista
    
    ---------
    
    Co-authored-by: Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
    
    * Complete quiroz-7 Desarrollador2
    
    * CartManager
    
    * Complete quiroz-7
    
    ---------
    
    Co-authored-by: Guada <110698501+Guada-pdls@users.noreply.github.com>
    
    * limpieza views (#32)
    
    ---------
    
    Co-authored-by: Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
    Co-authored-by: Israel <isra_quirozz@hotmail.com>

--- src/dao/models/Product.js
+++ src/dao/models/Product.js
@@ -1,17 +1,31 @@
 import { model, Schema } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
 
 let collection = "products";
 
 let schema = new Schema({
-  name: { type: String, required: true }, // unique?
+  name: { type: String, required: true, unique: true, index: true },
   description: { type: String, required: true },
-  category: { type: String, required: true },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      "Lightning",
+      "Wall Deco",
+      "Accessories",
+      "Textile",
+      "Art",
+      "Nature",
+      "Furniture",
+    ],
+  },
   price: { type: Number, required: true },
-  thumbnail: { type: String, required: true }, // investigar como hacer para tener las imágenes
-  code: { type: String, required: true, unique: true }, // Lo quitamos?
-  stock: { type: Number, required: true },
-  rating: { type: Number, default: 0 }, // required?
+  thumbnail: { type: String, required: true },
+  stock: { type: Number, default: 25 },
+  rating: { type: Number, default: 0 },
 });
+
+schema.plugin(mongoosePaginate);
 
 let Product = model(collection, schema);
 
