6ebb2b7f4db425c6b91dfa4f7e4930cea527900f
Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
2023-06-24 00:15:21 +0200

    Sessions (#38)
    * Complete hands on lab 2
    
    * Complete sprint-8
    
    * session issues
    
    * Resolve authentication issues

--- src/middlewares/authenticateAdmin.js
+++ src/middlewares/authenticateAdmin.js
@@ -2,10 +2,10 @@ const authenticateAdmin = (req, res, next) => {
   if (req.session.email !== "admin@admin.com" && req.session.role !== 1) {
     return res.status(400).json({
       success: false,
-      response: "buh!",
+      message: "Buh! You can`t access this endpoint",
     });
   }
-  next();
+  return next();
 };
 
 export default authenticateAdmin;
