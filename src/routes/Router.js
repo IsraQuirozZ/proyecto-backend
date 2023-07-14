e8b00ad25711765b9e9020eca93a1d88a6e6e4ee
Guada <110698501+Guada-pdls@users.noreply.github.com>
2023-07-14 12:19:37 -0300

    Piva 10 (#49)
    * login
    
    * logout

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,63 +1,64 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 
 class MainRouter {
-    constructor() {
-        this.router = Router()
-        this.init()
-    }
+	constructor() {
+		this.router = Router()
+		this.init()
+	}
 
-    getRouter() {
-        return this.router
-    }
+	getRouter() {
+		return this.router
+	}
 
-    init(){}
+	init() { }
 
-    applyCallbacks(callbacks) {
-        return callbacks.map(cb => async (...params) => {
-            try {
-                await cb.apply(this, params)
-            } catch (error) {
-                console.error(error)
-                params[1].status(500).send(error)
-            }
-        })
-    }
+	applyCallbacks(callbacks) {
+		return callbacks.map(cb => async (...params) => {
+			try {
+				await cb.apply(this, params)
+			} catch (error) {
+				console.error(error)
+				params[1].status(500).send(error)
+			}
+		})
+	}
 
-    generateCustomResponses = (req, res, next) => {
-        res.sendSuccess = payload => res.send({ status: 'success', payload })
-        res.sendServerError = error => res.send({ status: 'error', error })
-        res.sendUserError = error => res.send({ status: 'error', error })
-        next()
-    }
+	generateCustomResponses = (req, res, next) => {
+		res.sendSuccess = payload => res.send({ status: 'success', payload })
+		res.sendServerError = error => res.send({ status: 'error', error })
+		res.sendUserError = error => res.send({ status: 'error', error })
+		next()
+	}
 
-    hanldePolicies = policies => (req, res, next) => {
-        if (policies[0] === 'PUBLIC') return next()
-        const authHeader = req.headers.authorization
-        if (!authHeader) return res.status(401).send({ status: 'error', error: 'Unauthorized'})
-        const token = authHeader.split(' ')[0]
-        let user = jwt.verify(token, process.env.SECRET)
-        if (!policies.includes(user.role.toUpperCase())) {
-            return res.status(403).send({ status: 'error', error: 'Unauthorized'})
-        }
-        req.user = user
-        next()
-    }
+	hanldePolicies = policies => (req, res, next) => {
+		if (policies[0] === 'PUBLIC') return next()
+		if (!req.cookies.token) {
+			return res.status(401).send({ status: 'error', error: 'Unauthenticated' })
+		}
+		let user = jwt.verify(req.cookies.token, process.env.SECRET_JWT)
+		if (!policies.includes(user.role.toUpperCase())) {
+			return res.status(403).send({ status: 'error', error: 'Unauthorized' })
+		}
+		req.user = user
+		next()
+	}
 
-    get(path, policies, ...callbacks) {
-        this.router.get(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
-    }
+	get(path, policies, ...callbacks) {
+		this.router.get(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
+	}
 
-    post(path, policies, ...callbacks) {
-        this.router.post(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
-    }
+	post(path, policies, ...callbacks) {
+		this.router.post(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
+	}
 
-    put(path, policies, ...callbacks) {
-        this.router.put(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
-    }
+	put(path, policies, ...callbacks) {
+		this.router.put(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
+	}
 
-    delete(path, policies, ...callbacks) {
-        this.router.delete(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
-    }
+	delete(path, policies, ...callbacks) {
+		this.router.delete(path, this.hanldePolicies(policies), this.generateCustomResponses, this.applyCallbacks(callbacks))
+	}
 
 }
 
