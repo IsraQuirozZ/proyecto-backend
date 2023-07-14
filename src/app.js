efbeb2ae52c619be493de3d3140aeb7dca899042
Guada <110698501+Guada-pdls@users.noreply.github.com>
2023-07-13 23:55:13 -0300

    login (#45)

--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ import cors from "cors";
 import session from "express-session";
 import MongoStore from "connect-mongo";
 import passport from "passport";
-import inicializePassport from "./config/passport.js";
+import inicializePassport from './passport-jwt/passport.config.js';
 import cookieParser from "cookie-parser";
+import UserRouter from "./routes/UserRouter.js";
+import SessionRouter from "./routes/SessionRouter.js";
+import ProductsRouter from "./routes/ProductsRouter.js";
 
 const server = express();
-
 // Middlewares
 server.use(
   cors({
@@ -22,26 +24,29 @@ server.use(
 );
 
 server.use(cookieParser())
-server.use(
-  session({
-    secret: process.env.SECRET_SESSION,
-    resave: true,
-    saveUninitialized: true,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGO_LINK,
-      ttl: 7 * 24 * 60 * 60 * 1000,
-    }),
-    rolling: true,
-  })
-);
+// server.use(
+//   session({
+//     secret: process.env.SECRET_SESSION,
+//     resave: true,
+//     saveUninitialized: true,
+//     store: MongoStore.create({
+//       mongoUrl: process.env.MONGO_LINK,
+//       ttl: 7 * 24 * 60 * 60 * 1000,
+//     }),
+//     rolling: true,
+//   })
+// );
 server.use("/public", express.static("public"));
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
+
 inicializePassport();
 server.use(passport.initialize());
-server.use(passport.session());
+// server.use(passport.session());
 
-//
+server.use('/api/session', SessionRouter.getRouter())
+server.use('/api/users', UserRouter.getRouter())
+server.use('/api/products', ProductsRouter.getRouter())
 server.use("/", router); // Enrutador principal
 server.use(errorHandler); // Manejador de errores
 server.use(notFoundHandler); // Manejador de rutas inextistetes
--- src/middlewares/passportCall.js
+++ src/middlewares/passportCall.js
@@ -1,14 +1,12 @@
 import passport from "passport";
 
-export default (strategy) => {
+export default strategy => {
   return async (req, res, next) => {
     passport.authenticate(
       strategy,
       (err, user, info) => {
         if (err) next(err)
-        if (!user) {
-          return res.status(401).json({ error: info.toString() })
-        }
+        if (!user) { return res.status(401).json({ error: info.toString() }) }
         req.user = user
         return next()
       }
