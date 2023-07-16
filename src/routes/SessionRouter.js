bd57ed9494662e31ae3e767275b986255ec75798
Guada <110698501+Guada-pdls@users.noreply.github.com>
2023-07-16 02:09:09 -0300

    Piva 10 (#52)
    * login
    
    * logout
    
    * register
    
    * status code and login response
    
    * verify password

--- src/routes/SessionRouter.js
+++ src/routes/SessionRouter.js
@@ -6,16 +6,23 @@ import authJwt from "../passport-jwt/authJwt.js";
 import password_validator from "../middlewares/passwordValidator.js";
 import validator from "../middlewares/registerValidator.js";
 import createhash from "../middlewares/createhash.js";
+import { compareSync } from "bcrypt";
 
 class SessionRouter extends MainRouter {
 	init() {
 		this.post('/login', ['PUBLIC'], async (req, res) => {
 			try {
 				if (req.cookies.token) {
-					return res.sendUserError(400, 'You are already logged in')
+					return res.sendUserError(401, 'You are already logged in')
 				}
 				let user = await User.findOne({ email: req.body.email })
 				if (!user) { return res.sendUserError(404, 'User not registred') }
+
+				const { password } = user
+				let verified = compareSync(req.body.password, password)
+
+				if (!verified) { return res.sendUserError(400, 'Invalid email or password')}
+
 				let token = jwt.sign({ email: user.email, role: user.role }, process.env.SECRET_JWT)
 				res.cookie('token', token, {
 					maxAge: 60 * 60 * 24 * 7,
@@ -26,6 +33,7 @@ class SessionRouter extends MainRouter {
 				res.sendServerError(500, error)
 			}
 		})
+
 
 		this.post('/register', ['PUBLIC'],validator, password_validator, createhash, passportCall('register'), (req, res) => {
 			return res.sendSuccess('User registred successfully')
