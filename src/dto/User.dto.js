706a0ea2594423fd800428d4a037339f620e3f29
Guada <guadapdls@gmail.com>
2023-08-02 17:26:39 -0300

    ticket

--- src/dto/User.dto.js
+++ src/dto/User.dto.js
@@ -3,8 +3,9 @@ class UserDTO {
         this.first_name = user.first_name 
         this.last_name = user.last_name 
         this.full_name = user.first_name + ' ' + user.last_name
-        this.email = user.email 
-        this.password = user.password 
+        this.email = user.email
+        this.role = user.role
+        this.cid = user.cid
     }
 }
 
--- src/routes/CartRouter.js
+++ src/routes/CartRouter.js
@@ -1,13 +1,15 @@
 import MainRouter from "./Router.js";
 import CartController from "../controllers/CartController.js";
 
-const { getCart, addProducts, deleteProducts } = CartController
+const { getCart, addProduct, deleteProduct, deleteAllProducts, purchase } = CartController
 
 class CartRouter extends MainRouter {
     init() {
         this.get('/:cid', ['USER'], getCart)
-        this.put('/:cid/product/:pid/:units', ['USER'], addProducts)
-        this.put('/:cid/product/:pid/:units', ['USER'], deleteProducts)
+        this.post('/:cid/purchase', ['USER'], purchase)
+        this.put('/:cid/product/:pid/:units', ['USER'], addProduct)
+        this.put('/:cid/product/:pid/:units', ['USER'], deleteProduct)
+        this.delete('/:cid/clear', ['USER'], deleteAllProducts)
     }
 }
 
