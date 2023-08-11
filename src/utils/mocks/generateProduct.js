2766f8d47ddf902ea13eaa14d80135d85c6650c9
Guada <guadapdls@gmail.com>
2023-08-10 21:39:52 -0300

    product category

--- src/utils/mocks/generateProduct.js
+++ src/utils/mocks/generateProduct.js
@@ -1,11 +1,25 @@
 import { faker } from "@faker-js/faker";
 
+const getRandomCategory = () => {
+  const categories = [
+    "Lightning",
+    "Wall Deco",
+    "Accessories",
+    "Textile",
+    "Art",
+    "Nature",
+    "Furniture",
+  ];
+  const randomIndex = faker.number.int({ min: 0, max: categories.length - 1 });
+  return categories[randomIndex];
+};
+
 const generateProduct = () => {
   return {
     _id: faker.database.mongodbObjectId(),
     name: faker.commerce.productName(),
     description: faker.commerce.productDescription(),
-    category: faker.commerce.department(),
+    category: getRandomCategory(),
     price: faker.commerce.price(),
     thumbnail: faker.image.url(),
     stock: faker.string.numeric({ min: 5, max: 100 }),
