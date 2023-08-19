a90e2d8577e350db8de0f378ce588e69fa149f01
Guada <guadapdls@gmail.com>
2023-08-19 13:50:41 -0300

    scalability

--- src/server.js
+++ src/server.js
@@ -1,37 +1,78 @@
-import server from "./app.js";
-import { Server } from "socket.io";
-import chatManager from "./dao/managers/Chat.js";
+// import server from "./app.js";
+// import { Server } from "socket.io";
+// import chatManager from "./dao/managers/Chat.js";
 
-// const port = process.env.PORT || 8080;
-// const ready = () => {
-//   console.log("server ready on port " + port);
-//   connect(process.env.MONGO_LINK)
-//     .then(() => console.log("connected to db"))
-//     .catch((err) => console.log(err));
-// };
+// // SOCKET
 
-let http_server = server.listen(port, ready);
+// let socket_server = new Server(http_server);
+// const chats = chatManager.getChats();
 
-// SOCKET
+// socket_server.on("connection", (socket) => {
+//   // //  console.log(`client ${socket.client.id} connected`)
 
-let socket_server = new Server(http_server);
-const chats = chatManager.getChats();
+//   // Chat
 
-socket_server.on("connection", (socket) => {
-  // //  console.log(`client ${socket.client.id} connected`)
+//   socket.on("auth", () => {
+//     socket_server.emit("all_messages", chats); // Envía los mensajes cuando se autentique
+//   });
 
-  // Chat
+//   socket.on("new_message", (data) => {
+//     chatManager.addMessage(data);
+//     socket_server.emit("all_messages", chats);
+//   });
 
-  socket.on("auth", () => {
-    socket_server.emit("all_messages", chats); // Envía los mensajes cuando se autentique
-  });
+//   // Carrito
 
-  socket.on("new_message", (data) => {
-    chatManager.addMessage(data);
-    socket_server.emit("all_messages", chats);
-  });
+//   socket.emit("quantity", quantity);
+// });
+import express from "express";
+import "dotenv/config.js";
+import errorHandler from "./middlewares/error/errorHandler.js";
+import notFoundHandler from "./middlewares/notFoundHandler.js";
+import { __dirname } from "./utils/utils.js";
+import router from "./routes/index.js";
+import cors from "cors";
+import passport from "passport";
+import inicializePassport from "./passport-jwt/passport.config.js";
+import cookieParser from "cookie-parser";
+import config from "./config/config.js";
+import { addLogger, logger } from "./config/logger.js";
+import session from "express-session";
 
-  // Carrito
+const server = express();
+config.connectDB();
+server.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-  socket.emit("quantity", quantity);
+server.use(cookieParser());
+server.use("/public", express.static("public"));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+inicializePassport();
+server.use(passport.initialize());
+
+server.use(session({
+  secret: config.SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+server.use(addLogger)
+
+server.use('/', router)
+
+server.use(errorHandler)
+server.use(notFoundHandler)
+
+export const httpServer = () => server.listen(config.PORT, error => {
+  if (error) logger.error(error.message)
+  logger.info("Server listening on port " + config.PORT)
 });
+
+export default server;
