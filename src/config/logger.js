6356cac5c8d4c25bc314de50e954feaba99a7ca8
Guada <guadapdls@gmail.com>
2023-08-19 13:44:59 -0300

    logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,40 +1,62 @@
 import winston from "winston";
 import config from "./config.js";
 
-const customLevelOptions = {
-	levels: {
-		fatal: 0,
-		error: 1,
-		warning: 2,
-		info: 3,
-		debug: 4,
-	},
-	colors: {
-		fatal: 'red',
-		error: 'yellow',
-		warning: 'yellow',
-		info: 'blue',
-		debug: 'white'
-	}
-}
+const customLoggerOptions = {
+  levels: {
+    fatal: 0,
+    error: 1,
+    warning: 2,
+    info: 3,
+    http: 4,
+    debug: 5,
+  },
+  colors: {
+    fatal: "red",
+    error: "red",
+    warning: "yellow",
+    info: "green",
+    http: "blue",
+    debug: "magenta",
+  },
+  transports: {
+    console: {},
+  },
+};
 
-const logger = winston.createLogger({
-	levels: customLevelOptions.levels,
-	transports: [
-		new winston.transports.Console({
-			level: 'info',
-			format: winston.format.combine(
-				winston.format.colorize({ colors: customLevelOptions.colors }),
-				winston.format.simple()
-			)
-		}),
-		new winston.transports.File({
-			filename: 'errors.log',
-			level: 'warning',
-			format: winston.format.simple()
-		})
-	]
-})
+let logger;
+if (config.MODE === "development") {
+  logger = winston.createLogger({
+    levels: customLoggerOptions.levels,
+    transports: [
+      new winston.transports.Console({
+        level: "debug",
+        format: winston.format.combine(
+          winston.format.colorize({ colors: customLoggerOptions.colors }),
+          winston.format.simple()
+        ),
+      }),
+    ],
+  });
+} else {
+  logger = winston.createLogger({
+    levels: customLoggerOptions.levels,
+    transports: [
+      new winston.transports.Console({
+        level: "info",
+        format: winston.format.combine(
+          winston.format.colorize({ colors: customLoggerOptions.colors }),
+          winston.format.simple()
+        ),
+      }),
+
+      new winston.transports.File({
+        filename: "errors.log",
+        level: "error",
+        format: winston.format.simple(),
+      }),
+    ],
+  });
+}
 
 const addLogger = (req, res, next) => {
   req.logger = logger;
